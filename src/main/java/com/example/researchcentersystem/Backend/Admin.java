590bb1a0b765cf454263b556e763478a286fb376
budoor99 <bobo200259@gmail.com>
2023-12-06 12:53:32 +0300

    Add Admin class

---
+++ src/main/java/com/example/researchcentersystem/Backend/Admin.java
@@ -0,0 +1,8 @@
+package com.example.researchcentersystem.Backend;
+
+public class Admin extends User {
+
+    public Admin(String userName, String userEmail, String userType) {
+        super(userName, userEmail, userType);
+    }
+}
