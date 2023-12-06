ac2bba42fc4f3d835d92d2f28f796fa4fec45afd
Najla <jaloudnajla087@gmail.com>
2023-12-06 12:27:10 +0300

    adding machine class

---
+++ src/main/java/com/example/researchcentersystem/Backend/Machine.java
@@ -0,0 +1,10 @@
+package com.example.researchcentersystem.Backend;
+
+public class Machine {
+
+    private int machineID;
+    private String machineName;
+
+    private String[] researchInterest;
+
+}
