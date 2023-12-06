6d41b7a91cd330d0e9a23ec6e64b43f464d0232a
Najla <jaloudnajla087@gmail.com>
2023-12-06 13:05:36 +0300

    complete Reservation class

--- src/main/java/com/example/researchcentersystem/Backend/Reservation.java
+++ src/main/java/com/example/researchcentersystem/Backend/Reservation.java
@@ -7,7 +7,36 @@ public class Reservation {
     private LocalDateTime timeFrom;
     private LocalDateTime timeTo;
     private LocalDate day;
-    
 
+    public void reserveMachine(Machine machine1, LocalDateTime timeFrom, LocalDateTime timeTo){}
 
+    public Reservation(LocalDateTime timeFrom, LocalDateTime timeTo, LocalDate day) {
+        this.timeFrom = timeFrom;
+        this.timeTo = timeTo;
+        this.day = day;
+    }
+
+    public LocalDateTime getTimeFrom() {
+        return timeFrom;
+    }
+
+    public LocalDateTime getTimeTo() {
+        return timeTo;
+    }
+
+    public LocalDate getDay() {
+        return day;
+    }
+
+    public void setTimeFrom(LocalDateTime timeFrom) {
+        this.timeFrom = timeFrom;
+    }
+
+    public void setTimeTo(LocalDateTime timeTo) {
+        this.timeTo = timeTo;
+    }
+
+    public void setDay(LocalDate day) {
+        this.day = day;
+    }
 }
